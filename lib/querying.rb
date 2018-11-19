6f8fa57bf75e147a80bacb52e8217403d758a769
Dana Palmer <roiai4@hotmail.com>
2018-11-19 07:09:19 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, series.subgenre FROM authors, subgenre JOIN series.author_id = author.name JOIN series.subgenre_id = subgenres,name;"
 end
 
 def select_series_title_with_most_human_characters
