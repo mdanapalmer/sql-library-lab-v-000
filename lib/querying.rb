34b345b961a8b2b2f4979ea771831444e2231749
Dana Palmer <roiai4@hotmail.com>
2018-11-19 07:16:57 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -15,8 +15,8 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  SELECT series.title
-      "FROM series 
+  "SELECT series.title
+      FROM series
       JOIN books
       ON books.series_id = series.id
       JOIN character_books
